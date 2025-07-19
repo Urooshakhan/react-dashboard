fde059a64722e4e7618bf5c8994804ecfb6f218c
Urooshakhan <148043945+Urooshakhan@users.noreply.github.com>
2025-07-19 20:51:01 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   
   plugins: [react()],
-  base: '/react-dashboard/'
+  base: '/react-dashboard/',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 })
